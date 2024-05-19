88605b25e980e8ea72435dc1e5b46207b044ef36
TheRABU <fazlerabbi114@gmail.com>
2024-05-19 14:32:54 +0600

    created new api for fethcing user's uploaded single product

--- index.js
+++ index.js
@@ -249,6 +249,13 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.get("/user-view-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await myProductCollections.findOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
